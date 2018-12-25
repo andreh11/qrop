a853c7f4d3eed57f378afd091de9403f93bbc3f7
André Hoarau <ah@ouvaton.org>
2018-12-25 21:33:45 +0100

    Add year checking in TaskModel

--- core/taskmodel.cpp
+++ core/taskmodel.cpp
@@ -168,21 +168,23 @@ bool TaskModel::isDone(int row, const QModelIndex &parent) const
 {
     QDate completedDate = fieldDate(row, parent, "completed_date");
     bool completed = completedDate.isValid();
-    return completed && m_mondayDate <= completedDate && completedDate <= m_sundayDate;
+    return completed && m_mondayDate <= completedDate && completedDate <= m_sundayDate
+            && completedDate.year() == m_year;
 }
 
 bool TaskModel::isDue(int row, const QModelIndex &parent) const
 {
     QDate assignedDate = fieldDate(row, parent, "assigned_date");
     bool completed = rowValue(row, parent, "completed_date").toString() != "";
-    return !completed && m_mondayDate <= assignedDate && assignedDate <= m_sundayDate;
+    return !completed && m_mondayDate <= assignedDate && assignedDate <= m_sundayDate
+            && assignedDate.year() == m_year;
 }
 
 bool TaskModel::isOverdue(int row, const QModelIndex &parent) const
 {
     QDate assignedDate = fieldDate(row, parent, "assigned_date");
     bool completed = rowValue(row, parent, "completed_date").toString() != "";
-    return !completed && assignedDate < m_mondayDate;
+    return !completed && assignedDate < m_mondayDate && assignedDate.year() == m_year;
 }
 
 bool TaskModel::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
