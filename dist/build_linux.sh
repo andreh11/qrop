740e5e8942412d44016c79428463ad2c055add4d
André Hoarau <ah@ouvaton.org>
2019-01-06 13:59:35 +0100

    Add Linux deploy script

---
+++ dist/build_linux.sh
@@ -0,0 +1,19 @@
+##!/usr/bin/env bash
+
+mkdir build;
+cd build;
+qmake ..;
+make -j 8;
+mkdir -p deploy/usr/{bin, lib, share};
+mkdir usr/share/applications;
+find build \( -name "moc_*" -or -name "*.o" -or -name "qrc_*" -or -name "Makefile*" -or -name "*.a" \) -exec rm {} \;
+cp -R build/{core,desktop}/* deploy/usr/bin
+cd desploy;
+cp ../../logo.png desktop.png
+cp ../../dist/Qrop.desktop usr/share/applications
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage";
+chmod a+x linuxdeployqt*.AppImage;
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH;
+./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=/home/ah/src/qrop/desktop/qml -bundle-non-qt-libs -extra-plugins=sqldrivers,imageformats/libqsvg.s
+./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=/home/ah/src/qrop/desktop/qml -appimage
+find . | grep AppImage;
