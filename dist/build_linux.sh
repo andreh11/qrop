d29eeb9e0abb010cae10b3cf222ae698c07f5852
André Hoarau <ah@ouvaton.org>
2019-01-09 10:22:53 +0100

    Update .travis-ci.yml

--- dist/build_linux.sh
+++ dist/build_linux.sh
@@ -1,5 +1,6 @@
 ##!/usr/bin/env bash
 
+BUILD_DIR=$PWD
 mkdir build;
 cd build;
 qmake -config release ..;
@@ -14,6 +15,6 @@ cp ../../dist/Qrop.desktop usr/share/applications
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/5/linuxdeployqt-5-x86_64.AppImage"
 chmod a+x linuxdeployqt*.AppImage;
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH;
-./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=/home/ah/src/qrop/desktop/qml -bundle-non-qt-libs -extra-plugins=sqldrivers,imageformats/libqsvg.so
-./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=/home/ah/src/qrop/desktop/qml -appimage
+./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=$BUILD_DIR/desktop/qml -bundle-non-qt-libs -extra-plugins=sqldrivers,imageformats/libqsvg.so
+./linuxdeployqt*.AppImage usr/share/applications/Qrop.desktop -verbose=2 -qmldir=$BUILD_DIR/desktop/qml -appimage
 find . | grep AppImage;
