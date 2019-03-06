e16bb05047692cfc952da5f585ab328f7005421b
André Hoarau <ah@ouvaton.org>
2019-03-06 18:17:50 +0100

    Modify core_global.h for shared linkink on Windows

--- core/core_global.h
+++ core/core_global.h
@@ -4,17 +4,23 @@
 #include <QtCore/qglobal.h>
 
 #if defined(CORE_LIBRARY)
-#if defined(Q_OS_WIN)
-#define CORESHARED_EXPORT
-#else
 #define CORESHARED_EXPORT Q_DECL_EXPORT
-#endif
-#else
-#if defined(Q_OS_WIN)
-#define CORESHARED_EXPORT
 #else
 #define CORESHARED_EXPORT Q_DECL_IMPORT
 #endif
-#endif
+
+//#if defined(CORE_LIBRARY)
+//#if defined(Q_OS_WIN)
+//#define CORESHARED_EXPORT
+//#else
+//#define CORESHARED_EXPORT Q_DECL_EXPORT
+//#endif
+//#else
+//#if defined(Q_OS_WIN)
+//#define CORESHARED_EXPORT
+//#else
+//#define CORESHARED_EXPORT Q_DECL_IMPORT
+//#endif
+//#endif
 
 #endif // CORE_GLOBAL_H
