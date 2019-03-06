d1d51006598f94bfd731c177cefa439f724bf0c5
André Hoarau <ah@ouvaton.org>
2019-03-06 18:48:33 +0100

    Add core_global.h to CMakeLists

--- core/core_global.h
+++ core/core_global.h
@@ -9,18 +9,4 @@
 #define CORESHARED_EXPORT Q_DECL_IMPORT
 #endif
 
-//#if defined(CORE_LIBRARY)
-//#if defined(Q_OS_WIN)
-//#define CORESHARED_EXPORT
-//#else
-//#define CORESHARED_EXPORT Q_DECL_EXPORT
-//#endif
-//#else
-//#if defined(Q_OS_WIN)
-//#define CORESHARED_EXPORT
-//#else
-//#define CORESHARED_EXPORT Q_DECL_IMPORT
-//#endif
-//#endif
-
 #endif // CORE_GLOBAL_H
