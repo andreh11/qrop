7677fdd35ed5a7611a8d0d34fee3c7500767d7d9
André Hoarau <ah@ouvaton.org>
2019-10-07 11:42:19 +0200

    update .gitlab-ci.yml

--- dist/linux/before_install.sh
+++ dist/linux/before_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update -qq -y
-apt-get install -qq -y wget tree libglib2.0-0 libicu-dev libicu55 \
+sudo apt-get update -qq -y
+sudo apt-get install -qq -y wget tree libglib2.0-0 libicu-dev libicu55 \
                        libegl1-mesa
