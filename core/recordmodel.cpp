5f5351b186ca3ec9995f9c8c5e3eb34b2c2922b2
André Hoarau <ah@ouvaton.org>
2020-02-09 22:44:04 +0100

    add recordmodel

---
+++ core/recordmodel.cpp
@@ -0,0 +1,27 @@
+#include "recordmodel.h"
+
+#include <QDebug>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QSqlQuery>
+
+RecordModel::RecordModel(QObject *parent, const QString &tableName)
+    : SortFilterProxyModel(parent, tableName)
+{
+    sort(0);
+}
+
+// bool RecordModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+//{
+//    int leftCrop = sourceRowValue(left.row(), left.parent(), "planting_id").toInt();
+//    int rightCrop = sourceRowValue(right.row(), right.parent(), "planting_id").toInt();
+
+//    return leftCrop < rightCrop;
+//}
+
+bool RecordModel::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
+{
+    QDate date = sourceFieldDate(sourceRow, sourceParent, "date");
+    bool isInYear = QDate(m_year, 1, 1) <= date && date <= QDate(m_year, 12, 31);
+    return isInYear && SortFilterProxyModel::filterAcceptsRow(sourceRow, sourceParent);
+}
---
+++ core/recordmodel.h
@@ -0,0 +1,31 @@
+#ifndef RECORDMODEL_H
+#define RECORDMODEL_H
+
+#include <QDate>
+
+#include "core_global.h"
+#include "sortfilterproxymodel.h"
+
+class CORESHARED_EXPORT RecordModel : public SortFilterProxyModel
+{
+    Q_OBJECT
+    //    Q_PROPERTY(int plantingId READ plantingId WRITE setPlantingId NOTIFY plantingIdChanged)
+
+public:
+    RecordModel(QObject *parent = nullptr, const QString &tableName = "record_view");
+    //    bool lessThan(const QModelIndex &left, const QModelIndex &right) const override;
+
+    //    int plantingId() const;
+    //    void setPlantingId(int plantingId);
+
+    // signals:
+    //    void plantingIdChanged();
+
+protected:
+    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override;
+
+    // private:
+    //    int m_plantingId { -1 };
+};
+
+#endif // RECORDMODEL_H
