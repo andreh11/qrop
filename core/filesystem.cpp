cf65488cc1994665c8ffc1d26a369c64f3ffdfc5
André Hoarau <ah@ouvaton.org>
2021-01-08 08:11:53 +0100

    format and add submethods

--- core/filesystem.cpp
+++ core/filesystem.cpp
@@ -2,23 +2,48 @@
 #include <QStandardPaths>
 #include <QDebug>
 
-// QString FileSystem::s_rootPath = "/home/bruel/Documents/Qrop";
-QString FileSystem::s_rootPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
-//#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
-const QMap<QString, QString> FileSystem::s_subFolders = { { "csv", "csv" }, { "pdf", "pdf" } };
-//#endif
-
-FileSystem::FileSystem(QObject *parent)
-    : QObject(parent)
-{
-}
-
 //#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
 #define APP_NAME "Qrop"
 #include <QDir>
 #if defined(Q_OS_ANDROID)
 #include <QtAndroidExtras/QtAndroid>
 #endif
+
+QString FileSystem::s_rootPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+QString FileSystem::s_qropPath = QString("%1/%2").arg(s_rootPath, APP_NAME);
+const QMap<QString, QString> FileSystem::s_subFolders { { "csv", "csv" }, { "pdf", "pdf" } };
+
+FileSystem::FileSystem(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void FileSystem::createDocumentsFolder()
+{
+    if (QDir(s_rootPath).exists())
+        return;
+
+    qDebug() << "Creating Documents folder: " << s_rootPath;
+
+    QFileInfo fi(s_rootPath);
+    QDir dir(fi.absolutePath());
+
+    if (!dir.mkdir(fi.fileName())) {
+        qCritical() << "Couldn't create Documents folder...";
+        return;
+    }
+}
+
+void FileSystem::createQropFolder()
+{
+    QFileInfo fi(s_qropPath);
+    if (!fi.exists()) {
+        QDir dir(s_rootPath);
+        if (!dir.mkdir(APP_NAME))
+            qCritical() << "Couldn't create folder: " << fi.absoluteFilePath();
+    }
+}
+
 void FileSystem::createMobileRootFilesDirectories()
 {
     s_rootPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
@@ -31,37 +56,19 @@ void FileSystem::createMobileRootFilesDirectories()
     }
 #endif
 
-    // 1. Check if main Documents folder exist
-    if (!QDir(s_rootPath).exists()) {
-        qDebug() << "Creating Documents folder: " << s_rootPath;
-        QFileInfo fi(s_rootPath);
-        QDir dir(fi.absolutePath());
-        if (!dir.mkdir(fi.fileName())) {
-            qCritical() << "Couldn't create Documents folder...";
-            return;
-        }
-    }
-
-    // 2.: Create Qrop subFolder
-    QFileInfo fi(QString("%1/%2").arg(s_rootPath).arg(APP_NAME));
-    if (!fi.exists()) {
-        QDir dir(s_rootPath);
-        if (!dir.mkdir(APP_NAME))
-            qCritical() << "Couldn't create folder: " << fi.absoluteFilePath();
-    }
+    createDocumentsFolder();
+    createQropFolder();
 
     // 3.: create all subFolders
+    QFileInfo fi(s_qropPath);
     if (fi.exists()) {
-        s_rootPath = fi.absoluteFilePath();
-        QDir dir(s_rootPath);
-        for (const auto &folder : s_subFolders.values()) {
-            if (!QFileInfo(QString("%1/%2").arg(s_rootPath).arg(folder)).exists())
-                dir.mkdir(folder);
-        }
+        QDir dir(s_qropPath);
+        for (const auto &folder : s_subFolders.values())
+            dir.mkdir(folder);
     }
 }
 
-QStringList FileSystem::getAvailableDataBasesNames() const
+QStringList FileSystem::getAvailableDataBasesNames()
 {
     QStringList dbNames;
     QDir dir(s_rootPath);
--- core/filesystem.h
+++ core/filesystem.h
@@ -9,35 +9,46 @@ class CORESHARED_EXPORT FileSystem : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString rootPath READ rootPath)
-//#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
-    Q_PROPERTY(QString csvPath  READ csvPath)
-    Q_PROPERTY(QString pdfPath  READ pdfPath)
-//#endif
+    //#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
+    Q_PROPERTY(QString csvPath READ csvPath)
+    Q_PROPERTY(QString pdfPath READ pdfPath)
+    //#endif
 
 public:
     explicit FileSystem(QObject *parent = nullptr);
 
     inline QString rootPath() const;
 
-//#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
+    //#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
     inline QString csvPath() const;
     inline QString pdfPath() const;
 
-    Q_INVOKABLE QStringList getAvailableDataBasesNames() const;
+    Q_INVOKABLE static QStringList getAvailableDataBasesNames();
     Q_INVOKABLE QStringList getAvailableCsvFileNames() const;
 
     static void createMobileRootFilesDirectories();
-//#endif
+    //#endif
 
 private:
     static QString s_rootPath;
-//#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
+    static QString s_qropPath;
     static const QMap<QString, QString> s_subFolders;
-//#endif
+    static void createDocumentsFolder();
+    static void createQropFolder();
 };
 
-QString FileSystem::rootPath() const { return s_rootPath; }
+QString FileSystem::rootPath() const
+{
+    return s_rootPath;
+}
+
 //#if defined(Q_OS_ANDROID) || defined (Q_OS_IOS)
-QString FileSystem::csvPath() const { return QString("%1/%2").arg(s_rootPath).arg(s_subFolders.value("csv")); }
-QString FileSystem::pdfPath() const { return QString("%1/%2").arg(s_rootPath).arg(s_subFolders.value("pdf")); }
+QString FileSystem::csvPath() const
+{
+    return QString("%1/%2").arg(s_rootPath).arg(s_subFolders.value("csv"));
+}
+QString FileSystem::pdfPath() const
+{
+    return QString("%1/%2").arg(s_rootPath).arg(s_subFolders.value("pdf"));
+}
 //#endif
