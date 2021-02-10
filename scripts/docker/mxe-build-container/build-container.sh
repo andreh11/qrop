44aa5a58a568ed4148cd902bbbadcd7c56cfd3b1
André Hoarau <ah@ouvaton.org>
2021-02-10 08:51:21 +0100

    ci: add Dockers scripts for MXE build

---
+++ scripts/docker/mxe-build-container/build-container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+set -e
+
+# known good MXE sha
+MXE_SHA="8966a64"
+SCRIPTPATH=$(dirname $0)
+
+# version of the docker image
+VERSION=1.0
+
+pushd $SCRIPTPATH
+
+# we use the 'experimental' --squash argument to significantly reduce the size of the massively huge
+# Docker container this produces
+docker build --squash -t andreh11/mxe-build-container:$VERSION --build-arg=mxe_sha=$MXE_SHA -f Dockerfile .
+popd
---
+++ scripts/docker/mxe-build-container/settings.mk
@@ -0,0 +1,43 @@
+# This is a template of configuration file for MXE. See
+# index.html for more extensive documentations.
+
+# This variable controls the number of compilation processes
+# within one package ("intra-package parallelism").
+JOBS := 8
+
+# This variable controls the targets that will build.
+MXE_TARGETS :=  x86_64-w64-mingw32.shared i686-w64-mingw32.shared
+
+# The three lines below makes `make` build these "local packages" instead of all packages.
+# The ordering of the list appears weird, but this seems to help to get the build done
+# faster on a massively parallel machine to get some of the bottleneck packages built as
+# early as possible
+LOCAL_PKG_LIST := gcc \
+                  openssl \
+                  libmysqlclient \
+                  postgresql \
+                  qtbase \
+                  qtwebkit \
+                  nsis \
+                  curl \
+                  libxml2 \
+                  libxslt \
+                  libzip \
+                  libusb1 \
+                  hidapi \
+                  libgit2 \
+                  libftdi1 \
+                  mdbtools \
+                  qtconnectivity \
+                  qtdeclarative \
+                  qtimageformats \
+                  qtlocation \
+                  qtmultimedia \
+                  qtquickcontrols \
+                  qtquickcontrols2 \
+                  qtsvg \
+                  qttools \
+                  qttranslations \
+                  zstd
+.DEFAULT local-pkg-list:
+local-pkg-list: $(LOCAL_PKG_LIST)
