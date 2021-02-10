d685ae4c8c38ef06bebb641c9d32bd73f4b4f6cc
André Hoarau <ah@ouvaton.org>
2021-02-11 00:40:52 +0100

    use MXE packages

--- scripts/docker/mxe-build-container/build-container.sh
+++ scripts/docker/mxe-build-container/build-container.sh
@@ -4,15 +4,11 @@ set -e
 
 # known good MXE sha
 # MXE_SHA="8966a64"
-MXE_SHA="64b283d"
 SCRIPTPATH=$(dirname $0)
 
 # version of the docker image
-VERSION=1.0
+VERSION=5.15-static
 
 pushd $SCRIPTPATH
-
-# we use the 'experimental' --squash argument to significantly reduce the size of the massively huge
-# Docker container this produces
-docker build -t andreh11/mxe-build-container:$VERSION --build-arg=mxe_sha=$MXE_SHA -f Dockerfile .
+docker build -t andreh11/mxe-build-container:$VERSION -f Dockerfile .
 popd
