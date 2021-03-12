23f75d8338ca3d8198d9596db39684ecc5817ab5
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2021-03-12 20:21:58 +0100

    adding second test: TstFamilyService::testAddMethods using commands

--- tests/tst_familyservice.cpp
+++ tests/tst_familyservice.cpp
@@ -15,25 +15,45 @@ private slots:
     void init();
     void cleanup();
 
-    //    void initTestCase();
-    //    void cleanupTestCase();
-
     void testPrivateInsertionMethods();
+    void testAddMethods();
 
 private:
     void _testInitialFamilyServiceState(FamilyService *familySvc);
+    void _cleanupTestCase();
 };
 
 void TstFamilyService::init()
 {
-    qDebug() << "Init Qrop instance";
+    //    qDebug() << "Init Qrop instance";
     Qrop::instance()->initStatics();
 }
 
 void TstFamilyService::cleanup()
 {
-    qDebug() << "Clear Qrop instance";
+    //    qDebug() << "Clear Qrop instance";
     Qrop::instance()->clear();
+}
+
+void TstFamilyService::_cleanupTestCase()
+{
+    qDebug() << "running cleanup";
+    FamilyService *familySvc = Qrop::instance()->familyService();
+    familySvc->clear();
+    _testInitialFamilyServiceState(familySvc);
+}
+
+void TstFamilyService::_testInitialFamilyServiceState(FamilyService *familySvc)
+{
+    QVERIFY(familySvc != nullptr);
+    QVERIFY(qrp::SeedCompany::sLastId == 0);
+    QVERIFY(qrp::Family::sLastId == 0);
+    QVERIFY(qrp::Crop::sLastId == 0);
+    QVERIFY(qrp::Variety::sLastId == 0);
+    QVERIFY(familySvc->m_seedCompanies.size() == 0);
+    QVERIFY(familySvc->m_families.size() == 0);
+    QVERIFY(familySvc->m_crops.size() == 0);
+    QVERIFY(familySvc->m_varieties.size() == 0);
 }
 
 void TstFamilyService::testPrivateInsertionMethods()
@@ -57,7 +77,7 @@ void TstFamilyService::testPrivateInsertionMethods()
     QVERIFY(crop1 != nullptr);
     QVERIFY(crop1->family == fam1);
     QVERIFY(fam1->crops.size() == 1);
-    QVERIFY(fam1->crops.first() == crop1);
+    QVERIFY(fam1->crops.last() == crop1);
 
     // add 1 variety and do all checks
     qrp::Variety *var1 = familySvc->addVariety(qrp::Variety::getNextId(), false, "Variety 1",
@@ -66,24 +86,51 @@ void TstFamilyService::testPrivateInsertionMethods()
     QVERIFY(var1 != nullptr);
     QVERIFY(var1->crop == crop1);
     QVERIFY(crop1->varieties.size() == 1);
-    QVERIFY(crop1->varieties.first() == var1);
+    QVERIFY(crop1->varieties.last() == var1);
 
-    // check clear
-    familySvc->clear();
-    _testInitialFamilyServiceState(familySvc);
+    _cleanupTestCase();
 }
 
-void TstFamilyService::_testInitialFamilyServiceState(FamilyService *familySvc)
+void TstFamilyService::testAddMethods()
 {
-    QVERIFY(familySvc != nullptr);
-    QVERIFY(qrp::SeedCompany::sLastId == 0);
-    QVERIFY(qrp::Family::sLastId == 0);
-    QVERIFY(qrp::Crop::sLastId == 0);
-    QVERIFY(qrp::Variety::sLastId == 0);
-    QVERIFY(familySvc->m_seedCompanies.size() == 0);
-    QVERIFY(familySvc->m_families.size() == 0);
-    QVERIFY(familySvc->m_crops.size() == 0);
-    QVERIFY(familySvc->m_varieties.size() == 0);
+    FamilyService *familySvc = Qrop::instance()->familyService();
+    _testInitialFamilyServiceState(familySvc);
+
+    // add 2 families
+    int famId1 = familySvc->addNewFamily("Family 1", "black");
+    QVERIFY(famId1 == 1);
+    QVERIFY(familySvc->m_families.size() == 1);
+    qrp::Family *fam1 = familySvc->family(famId1);
+    QVERIFY(fam1 != nullptr);
+
+    int famId2 = familySvc->addNewFamily("Family 2", "black");
+    QVERIFY(famId2 == 2);
+    QVERIFY(familySvc->m_families.size() == 2);
+    qrp::Family *fam2 = familySvc->family(famId2);
+    QVERIFY(fam2 != nullptr);
+
+    // add 1 crop and do all checks
+    int cropId1 = familySvc->addNewCrop(famId1, "Crop 1 of Family 1", "black");
+    QVERIFY(cropId1 == 1);
+    QVERIFY(familySvc->m_crops.size() == 1);
+    qrp::Crop *crop1 = familySvc->crop(cropId1);
+    QVERIFY(crop1 != nullptr);
+    QVERIFY(crop1->family == fam1);
+    QVERIFY(fam1->crops.size() == 1);
+    QVERIFY(fam1->crops.last() == crop1);
+    QVERIFY(crop1->varieties.size() == 1); // Unknown one
+    QVERIFY(crop1->varieties.last()->name == tr("Unknown"));
+
+    // add a second variety and do all checks
+    int varId1 = familySvc->addNewVariety(crop1->id, "Variety 1", -1);
+    QVERIFY(varId1 == 2);
+    QVERIFY(familySvc->m_varieties.size() == 2);
+    qrp::Variety *var1 = familySvc->variety(varId1);
+    QVERIFY(var1 != nullptr);
+    QVERIFY(var1->crop == crop1);
+    QVERIFY(crop1->varieties.last() == var1);
+
+    _cleanupTestCase();
 }
 
 QTEST_APPLESS_MAIN(TstFamilyService)
